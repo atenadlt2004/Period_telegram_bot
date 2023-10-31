517c99c814420381ac92f99665bea5b9abb10c6d
Eliya kaheni <89583292+EliyaKaheni@users.noreply.github.com>
2023-11-01 01:20:06 +0330

    Update men-section bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import telebot
 from telebot import types
 import datetime
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot('TOKEN')
 
 # Variables
 name = ""
@@ -61,6 +61,48 @@ def start3(message):
     markup.add(item_yes, item_no)
     bot.reply_to(
         message, "آیا پارتنر عاطفی داری؟", reply_markup=markup)
+
+
+@bot.message_handler(func=lambda message: message.text == "مرد")
+def isman(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item_are = types.KeyboardButton("آره")
+    item_na = types.KeyboardButton("نه")
+    markup.add(item_are, item_na)
+    bot.reply_to(
+        message, "آیا پارتنر عاطفی داری؟", reply_markup=markup)
+
+
+@bot.message_handler(func=lambda message: message.text == "آره")
+def partnerofman(message):
+    bot.send_message(
+        message.chat.id, "(:یه پیام از پارتنرت واسم فوروارد کنم تا آیدیش رو داشته باشم. اینطوری میتونم بیشتر به تو و رابطت کمک کنم")
+    bot.register_next_step_handler(message, start11)
+
+
+def start11(message):
+    global partner_id
+    partner_id = message.forward_from.id
+    bot.send_message(message.chat.id, "لطفا اسم پارتنرت رو بهم بگو")
+    bot.register_next_step_handler(message, start12)
+
+
+@bot.message_handler(func=lambda message: message.text == "نه")
+def thesingleboy(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    bot.send_message(message.chat.id, "(:دوست عزیزم این بات برای آقایونی هست که پارتنر دارن...ببخش که نمیتونم در این زمینه راهنماییت کنم اما اگه دوست داری میتونی بازدن کلید پریود چیستت در منو اطلاعات زیادی راجع به پریود بدست بیاری")
+    item_resendinformation = types.KeyboardButton("تغییر اطلاعات")
+    item_whatisperid = types.KeyboardButton("پریود چیست")
+    markup.add(item_resendinformation, whatisperiod)
+    bot.reply_to(
+        message, "چطور میتونم کمکت کنم؟", reply_markup=markup)
+
+
+def start12(message):
+    global partner_name
+    partner_name = message.text
+    bot.send_message(
+        partner_id,  "(:سلام دوست عزیز من : من یک بات تلگرام هستم برای کمک به رابطه ی عاطفی شما و پارتنرت آیدیت رو ازش گرفتم ...ازآشنایی باهات خیلی خوشحالم")
 
 
 @bot.message_handler(func=lambda message: message.text == "بله")
